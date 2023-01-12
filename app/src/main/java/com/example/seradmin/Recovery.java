c78ad9616f56c454a9cd5d12c82037e0fba09f05
at6DAM2 <martinezmiguel417@gmail.com>
2023-01-12 17:09:28 +0100

    Empieza SerAdmin

--- app/src/main/java/com/example/seradmin/Recovery.java
+++ app/src/main/java/com/example/seradmin/Recovery.java
@@ -1,14 +1,14 @@
 package com.example.seradmin;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Recovery extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
     }
 }
